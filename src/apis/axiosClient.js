ded1a1423de8f6f82ffbd96b34f04c591eeb6111
QuanKhs <quanphamluong@gmail.com>
2021-08-15 22:25:28 +0700

    config axios cookies

--- src/apis/axiosClient.js
+++ src/apis/axiosClient.js
@@ -1,4 +1,3 @@
-// api/axiosClient.js
 import axios from 'axios';
 import queryString from 'query-string';
 // Set up default config for http requests here
@@ -6,13 +5,12 @@ import queryString from 'query-string';
 // Please have a look at here `https://github.com/axios/axios#request-config` for the full list of configs
 
 const axiosClient = axios.create({
-    baseURL: process.env.API_URL,
-    headers: {
-        'content-type': 'application/json',
-    },
+    baseURL: process.env.BASE_URL,
+    withCredentials: true,
+    headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
     paramsSerializer: params => queryString.stringify(params),
 });
-    
+
 
 axiosClient.interceptors.request.use(async (config) => {
     // Handle token here ...
@@ -25,6 +23,7 @@ axiosClient.interceptors.response.use((response) => {
     }
     return response;
 }, (error) => {
+    console.log('🚀 ~ file: axiosClient.js ~ line 27 ~ axiosClient.interceptors.response.use ~ error', error);
     // Handle errors
     throw error;
 });
